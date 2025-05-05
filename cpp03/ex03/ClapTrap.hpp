2d321cc9c7ad9ec86af74f350e4ba005388d404a
AmineSalmi01 <amine.salmi.solicode@gmail.com>
2025-05-05 22:28:02 +0100

    ex03

---
+++ cpp03/ex03/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+protected:
+	std::string name;
+	unsigned int hitPoints;
+	unsigned int energyPoints;
+	unsigned int attackDamage;
+public:
+	ClapTrap();
+	ClapTrap(const std::string& name);
+	~ClapTrap();
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+	std::string getName() const;
+	int getHitPoints() const;
+	int getEnergyPoints() const;
+	int getAttackDamage() const;
+};
