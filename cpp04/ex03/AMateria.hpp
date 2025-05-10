24ff8868af16da727ce3ebf043be93606d87a4cd
Amine Salmi <92025603+AmineSalmi01@users.noreply.github.com>
2025-05-10 15:22:03 +0000

    ex03

---
+++ cpp04/ex03/AMateria.hpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+class AMateria
+{
+    public:
+    AMateria(std::string const & type);
+    std::string const & getType() const;
+    virtual AMateria* clone() const = 0;
+    virtual void use(ICharacter& target);
+};
